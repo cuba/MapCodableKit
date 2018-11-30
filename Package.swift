8bc3b8b8e4758dceb815c76d0cbafe07ef1b7ea6
Jacob Sikorski <jacob.sikorski@gmail.com>
2018-11-30 02:17:48 -0500

    Fix `Package.swift`

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MapCodable",
+    dependencies: [
+    ],
     targets: [
         Target(name: "MapCodable")
-    ],
-    dependencies: [
     ]
 )

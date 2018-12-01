8d7acc8df871fde226d5006cebbfb24e0dca9acc
Jacob Sikorski <jacob.sikorski@gmail.com>
2018-12-01 18:54:43 -0500

    Move `MapPrimitive` to own file

--- Sources/MapCodable/Map.swift
+++ Sources/MapCodable/Map.swift
@@ -7,24 +7,6 @@
 
 import Foundation
 
-public protocol MapPrimitive {}
-
-extension String: MapPrimitive {}
-extension Double: MapPrimitive {}
-extension Int: MapPrimitive {}
-extension Int8: MapPrimitive {}
-extension Int16: MapPrimitive {}
-extension Int32: MapPrimitive {}
-extension Int64: MapPrimitive {}
-extension UInt: MapPrimitive {}
-extension UInt8: MapPrimitive {}
-extension UInt16: MapPrimitive {}
-extension UInt32: MapPrimitive {}
-extension UInt64: MapPrimitive {}
-extension Bool: MapPrimitive {}
-extension Array: MapPrimitive where Element: MapPrimitive {}
-extension Dictionary: MapPrimitive where Key: StringProtocol, Value: MapPrimitive {}
-
 public class Map {
     private var values: [String: Any?]
     
---
+++ Sources/MapCodable/MapPrimitive.swift
@@ -0,0 +1,26 @@
+//
+//  MapPrimitive.swift
+//  MapCodable
+//
+//  Created by Jacob Sikorski on 2018-12-01.
+//
+
+import Foundation
+
+public protocol MapPrimitive {}
+
+extension String: MapPrimitive {}
+extension Double: MapPrimitive {}
+extension Int: MapPrimitive {}
+extension Int8: MapPrimitive {}
+extension Int16: MapPrimitive {}
+extension Int32: MapPrimitive {}
+extension Int64: MapPrimitive {}
+extension UInt: MapPrimitive {}
+extension UInt8: MapPrimitive {}
+extension UInt16: MapPrimitive {}
+extension UInt32: MapPrimitive {}
+extension UInt64: MapPrimitive {}
+extension Bool: MapPrimitive {}
+extension Array: MapPrimitive where Element: MapPrimitive {}
+extension Dictionary: MapPrimitive where Key: StringProtocol, Value: MapPrimitive {}

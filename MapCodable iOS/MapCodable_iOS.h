bbcc775d42855dac26d7069feb996065aaeed070
Jacob Sikorski <jacob.sikorski@gmail.com>
2018-12-02 12:19:24 -0500

    Add iOS target

---
+++ MapCodable iOS/MapCodable_iOS.h
@@ -0,0 +1,18 @@
+//
+//  MapCodable_iOS.h
+//  MapCodable iOS
+//
+//  Created by Jacob Sikorski on 2018-12-02.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MapCodable_iOS.
+FOUNDATION_EXPORT double MapCodable_iOSVersionNumber;
+
+//! Project version string for MapCodable_iOS.
+FOUNDATION_EXPORT const unsigned char MapCodable_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapCodable_iOS/PublicHeader.h>
+
+
